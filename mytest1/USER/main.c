abf1ecb18274bcbd6c286a38894dba20ce5f3c1b
zhangcl <1830076667@qq.com>
2020-05-25 14:46:01 +0800

    v1

---
+++ mytest1/USER/main.c
@@ -0,0 +1,17 @@
+#include "delay.h"
+#include "usart.h"
+int main(void)
+{
+	u8 t=0;
+	delay_init();
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	uart_init(115200);
+	while(1)
+	{
+		printf("t:%d\n",t);
+		delay_ms(500);
+		t++;
+		/////////
+		//////////
+	}
+}
